3893ace5e79c6c9b750a0c4e598a10bea459ca25
Marshall Thompson <marshallthomp@gmail.com>
2016-10-12 23:27:30 -0400

    got a basic version working

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,9 +1,29 @@
-var $ = require('jQuery');
+var $ = require('jquery');
+var jQuery = require('jquery');
 var Handlebars = require('handlebars');
-var jHandlebars = require('./plugin.js');
+// var jHandlebars = require('./plugin');
+
+
+// the most basic implementation with alias protection
+(function($){
+
+  $.fn.template = function(templateId, contextObj) {
+    var source = $(templateId).html();
+    var template = Handlebars.compile(source);
+    var $html = $(template(contextObj));
+    return $(this).append($html);
+  };
+
+}(jQuery));
+
 
 /*
-  we want to use 
+  we want to leverage jquery to make handlebars templates easier
+  to use. the goal is below:
  */
-var context = {};
-jQuery('#target-container').template('#template-id', context);
+var context = {
+  heading: "jQuery Template",
+  body: "This is all the awesome"
+};
+
+$('#target-container').template('#template', context);
