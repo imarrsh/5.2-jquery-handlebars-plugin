265780ceadf9ba9ab5467c3d285a62d35f668475
Marshall Thompson <marshallthomp@gmail.com>
2016-10-12 20:55:46 -0400

    testing sublime git feature

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,3 +1,9 @@
 var $ = require('jQuery');
 var Handlebars = require('handlebars');
 var jHandlebars = require('./plugin.js');
+
+/*
+  we want to use 
+ */
+var context = {};
+jQuery('#target-container').template('#template-id', context);
